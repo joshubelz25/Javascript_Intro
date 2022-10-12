5b9ac803823d7898b8b0e5ff3ea945af69fee199
joshubelz25 <joshubelz25@g.crookcountyschools.org>
2022-10-12 14:36:36 -0700

    Added functions to the intro

---
+++ script.js
@@ -0,0 +1,49 @@
+//console.log("hello world");
+
+let animal = 'RAATSS!';
+
+console.log(animal);
+
+var number1 = 5;
+
+var number2 = 10;
+
+let math ="I have two numbers " + number1 + " and " + number2 + "Lets do some math with these numbers."
+
+function mathIntro(number1, number2){
+console.log(" have two numbers " + number1 + " and " + number2 + "Lets do some math with these numbers.")    
+}
+
+function addition(number1, number2){
+let equalToo = number1 + number2
+console.log("When added " + number1 + " plus " + number2 + " is equal to 7 ")
+}
+
+function subtraction(number1, number2){
+let equalToo = number1 - number2;
+console.log("When subtracted " + number1 + " minus " + number2 + " is equal to 3 ")    
+}
+function dividing(number1, number2){
+    let equalToo = number1 / number2;
+    console.log("When divided " + number1 + " divided by " + number2 + " is equal to 30 ")
+}
+function mulitplication(number1, number2){
+let equalToo = number1 * number2;
+console.log("When mulitplied " + number1 + " times " + number2 + " is equal to 30 ")
+}
+
+//let sum =number1 + number2;
+//let diff = number1 - number2;
+let mult = number1 * number2;
+let div = number2/number1;
+
+mathIntro(50, 1);
+addition(3, 4);
+subtraction(15, 12);
+dividing(15, 3);
+mulitplication(15, 2);
+
+console.log(mathIntro);
+console.log(mult);
+console.log(div);
+
